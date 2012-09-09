37a6b3d20f7deeb4074f82f419b0efd8bdc1a500
RussReynolds <russ@caterdeck.com>
2012-09-09 07:47:26 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaterdeckBetaPrelaunch::Application.config.secret_token = '0eed6b4cccabe19870935e0fcb3c46a9a29c2205072caf3cc9da4d45b613b7ac2ba2512d3cb804fe8d2b0005de92776dcb6fb7580ce39a79b5dbeca7330d6e37'
